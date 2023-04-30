7a62cc5f2b348b2e80f59548abd10916094b907a
Nathanael Mbale <66083904+nathanaelmbale@users.noreply.github.com>
2023-05-01 02:19:29 +0300

    cors middleware

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,14 @@ app.use((req, res, next) => {
     next()
 })
 
+//CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
+  
 //routes
 app.use('/user', userRoutes)
 app.use('/post', postRoutes)
